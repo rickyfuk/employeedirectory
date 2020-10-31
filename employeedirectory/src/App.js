599cc4dbb1da63d12b1fa5f244166ca232360687
digilau <rickyfukfuk@gmail.com>
2020-10-31 03:46:36 -0700

    add the search bar

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/header';
+import SearchBar from './components/SearchBar/searchBar';
 
 function App() {
+	const [searchTarget, setSearchTarget] = useState('');
+
+	function handleStaffSearching(event) {
+		setSearchTarget(event.target.value);
+	}
+
 	return (
 		<div className='App'>
 			<Header />
-			{/* <SearchBar /> */}
+			<SearchBar
+				staffSearching={handleStaffSearching}
+				searchTarget={searchTarget}
+			/>
 			{/* <ResultTable /> */}
 		</div>
 	);
---
+++ employeedirectory/src/components/SearchBar/searchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './searchBar.css';
+
+function SearchBar(props) {
+	return (
+		<div className='staffSearch'>
+			<div className='staffSearchTitle'>
+				<h4>Search Employee</h4>
+			</div>
+			<div className='input-group mb-3'>
+				<input
+					type='text'
+					className='form-control'
+					placeholder='Enter an employee name'
+					// the attribute "value" for saving the user input
+					value={props.searchTarget}
+					// the attribute "onChange" is an event listening where kicks the staffSearching function whenever the input is change
+					onChange={props.staffSearching}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default SearchBar;
