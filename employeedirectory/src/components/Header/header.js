13dae25f354a2bd7d3d1d84e2d812c15bf64b2fa
digilau <rickyfukfuk@gmail.com>
2020-10-31 02:51:41 -0700

    initial commit

---
+++ employeedirectory/src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+	return (
+		<div className='jumbotron header'>
+			<h1>Employee Directory</h1>
+		</div>
+	);
+}
+
+export default Header;
